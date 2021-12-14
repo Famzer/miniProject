1bc8f1152b7beeceb9aaa3124c8695ab09d8a3b0
IrfnDanwar <irfandik48@gmail.com>
2021-12-14 15:31:05 +0800

    miniProject

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentasion/coin_detail/CoinDetailState.kt
@@ -0,0 +1,9 @@
+package com.plcoding.cryptocurrencyappyt.presentasion.coin_list
+
+import com.plcoding.cryptocurrencyappyt.domain.model.CoinDetail
+
+data class CoinDetailState(
+    val isLoading: Boolean = false,
+    val coin: CoinDetail? = null,
+    val error: String = ""
+)
